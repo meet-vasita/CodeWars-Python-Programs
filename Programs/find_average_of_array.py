9943a9c4fc6a1c0d586575d4670cc268d1f49fa5
meet-vasita <meetvasita85@gmail.com>
2025-06-08 21:17:27 +0530

    Done

---
+++ Programs/find_average_of_array.py
@@ -0,0 +1,13 @@
+# Write a function which calculates the average of the numbers in a given array.
+
+# Note: Empty arrays should return 0.
+
+
+def find_average(numbers):
+        if not numbers:
+                return 0
+        return sum(numbers) / len(numbers)
+
+
+
+print(find_average([1,2,3]))
