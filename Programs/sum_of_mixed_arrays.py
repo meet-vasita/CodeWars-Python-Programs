b2d6c9c20fcbc51eed4f06848ddc0a3c9ec93877
meet-vasita <meetvasita85@gmail.com>
2025-06-10 13:06:44 +0530

    Done

---
+++ Programs/sum_of_mixed_arrays.py
@@ -0,0 +1,15 @@
+# Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+# Return your answer as a number.
+
+def sum_mix(arr):
+    total = 0
+    for x in arr:
+        total += int(x)
+    return total
+
+
+#--------------------
+
+def sum_mix(arr):
+    return sum(int(x) for x in arr)
