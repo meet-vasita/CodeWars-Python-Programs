bfae419f1c0d7e56082edeaa3a733fb3c33bfed6
meet-vasita <meetvasita85@gmail.com>
2025-06-17 16:45:27 +0530

    Done

---
+++ Programs/Friend_or_Foe.py
@@ -0,0 +1,20 @@
+# Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+# If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+# Input = ["Ryan", "Kieran", "Jason", "Yous"]
+# Output = ["Ryan", "Yous"]
+
+# Input = ["Peter", "Stephen", "Joe"]
+# Output = []
+
+
+def friend(x):
+    lst = []
+    for i in x:
+        if len(i) == 4:
+            lst.append(i)
+    return lst
+        
+
+print(friend(['Ryan','Foe']))
